50534e42944454039a540b6ff4178c46621df44e
feodorrussia <fvdelows@mail.ru>
2024-04-06 01:18:25 +0300

    Del old preproc realisation & log_df, also add multichannel preproc logic

--- source/Signal_processing.py
+++ source/Signal_processing.py
@@ -92,82 +92,6 @@ def length_preproc(preproc_fragments, rate,
     return np.array(result_fragments, dtype="object")
 
 
-def fft_butter_skewness_filtering(x_data, signal_data, rate):
-    """
-    :param x_data:  np.array(), main signal x_data
-    :param signal_data: np.array(), main signal y_data
-    :param rate: int, signal rate (4 / 10)
-    :return: [x_lists, y_lists], filtered fragments
-    """
-    # CONSTANTS
-    TOLERANCE = 1  # Чем выше, тем больше шанс получить два филамента на одной картинке
-    MIN_PERIODS = 4  # В среднем количество колебаний на графике, начальный порог
-    SINUSOIDALITY = 0.7  # Абсолютная асимметрия
-    BOARDERS_PERCENT = 0.5  # Сколько процентов длины добавляем слева и справа от филамента.
-    MIN_LENGTH_MCS = 0.008
-    MAX_LENGTH_MCS = 0.035
-
-    signal_data_d2 = np.diff(signal_data, n=2)
-
-    b, a = signal.butter(3, 0.4)
-
-    signal_data_d2 = signal.filtfilt(b, a, signal_data_d2)
-
-    # Просто выбираем в качестве аномалий то, что +- стандартное отклонение. В лоб, но может сработать
-    m = signal_data_d2.mean()
-    std = signal_data_d2.std()
-
-    # преобразование Фурье и получение массива частот
-    fft = np.fft.fft(signal_data_d2)
-    frequency = np.abs(np.fft.fftfreq(len(signal_data_d2)))
-
-    region = 3
-
-    y_d2_logic = np.zeros(len(signal_data_d2))
-
-    for i in range(3, len(y_d2_logic) - 3):
-        condition = False
-        for k in range(-region, region + 1):
-            condition = condition or ((signal_data_d2[i + k] > m + std) or (signal_data_d2[i + k] < m - std)) \
-                        and frequency[i] > 0.03
-        if condition:
-            y_d2_logic[i] = True
-
-    signal_data_f = np.array([signal_data[i] if y_d2_logic[i] else 0 for i in range(len(y_d2_logic))])
-
-    preprocessed = ";".join(map(str, signal_data_f)).split("0.0;" * TOLERANCE)
-    preprocessed = [i.split(";") for i in preprocessed if len(i) > 1]
-    for i in range(len(preprocessed)):
-        # print(preprocessed[i] if "-" in preprocessed[i] else "", end="")
-        preprocessed[i] = [float(j) for j in preprocessed[i] if j != "" and j != "-"]
-
-    filtered_data = length_preproc(preprocessed, rate)
-
-    fragments = [[], []]
-
-    for i in range(len(filtered_data)):
-        y_ = filtered_data[i]
-
-        k = 0
-        mean = np.mean(y_)
-        for j in range(len(y_) - 1):
-            if (y_[j] - mean) * (y_[j + 1] - mean) < 0:
-                k += 1
-
-        abs_skewness = np.abs(skew(y_))
-
-        if k > MIN_PERIODS * 2 and abs_skewness < SINUSOIDALITY:
-            r = x_in_y(filtered_data[i][:5].tolist(), signal_data.tolist())
-            x_id = np.array(
-                list(range(r - int(BOARDERS_PERCENT * filtered_data[i].shape[0]),
-                           r + int((BOARDERS_PERCENT + 1) * filtered_data[i].shape[0]))))
-
-            fragments[0].append(x_data[x_id])
-            fragments[1].append(signal_data[x_id])
-
-    return fragments
-
-
 @jit
 def count_fft_max(arr: np.array, arr2: np.array = None):
     f_increase = False
@@ -218,10 +142,10 @@ def periods_count(fragment):
     return k
 
 
-def fft_butter_skewness_filtering_new(t_data, signal_data, rate, log_df=None, f_disp=False):
+def fft_butter_skewness_filtering(t_data, signal_data, rate, f_disp=False):
     """
     :param t_data:  np.array(), main signal t_data
-    :param signal_data: np.array(), main signal y_data
+    :param signal_data: list(np.array()), channels list of signal y_data
     :param rate: int, signal rate (4 / 10)
     :param log_df: data frame for plotting app (testing)
     :return: [x_lists, y_lists], filtered fragments
@@ -239,24 +163,23 @@ def fft_butter_skewness_filtering_new(t_data, signal_data, rate, log_df=None, f_
     REGION_LENGTH_MCS = 0.012
     MAX_LENGTH_MCS = 0.035
 
+    CHANNELS_N = len(signal_data)
+
     region = (int(REGION_LENGTH_MCS * rate * 1000) + 1) // 2  # получаем количество точек для рассматриваемого "окна"
 
     if f_disp:
         print("|", end="")
 
     n_diff = 1
-    signal_data_d1 = np.diff(signal_data, n=n_diff)
+    signal_data_d1 = [np.diff(signal_data[i], n=n_diff) for i in range(CHANNELS_N)]
     # if log_df is not None:
     #     log_df[f"ch11_{n_diff}d"] = np.concatenate([np.diff(signal_data, n=n_diff), [0]*n_diff])
 
     b, a = signal.butter(5, 0.1)
 
-    signal_data_d1 = signal.filtfilt(b, a, signal_data_d1)
-    if log_df is not None:
-        log_df["ch11_b"] = np.concatenate([signal_data_d1, [0] * n_diff])
+    signal_data_d1 = [signal.filtfilt(b, a, signal_data_d1[i]) for i in range(CHANNELS_N)]
 
     preprocessed_ind_data = [[]]
-    signal_data_f = np.zeros(signal_data.shape[0])
     f_fragment = False
 
     search_step = region // 2
@@ -265,29 +188,32 @@ def fft_butter_skewness_filtering_new(t_data, signal_data, rate, log_df=None, f_
     point = region
     while point < signal_data.shape[0] - region - n_diff:
         # Выбираем в качестве аномалий то, что +- стандартное отклонение. В лоб, но может сработать
-        fragment_d1 = signal_data_d1[point - region:point + region]
+        f_point = False
+        for ch_i in range(CHANNELS_N):
+            fragment_d1 = signal_data_d1[ch_i][point - region:point + region]
 
-        periods_d1 = periods_count(fragment_d1)
-        periods = periods_count(signal_data[point - region:point + region])
+            periods_d1 = periods_count(fragment_d1)
+            periods = periods_count(signal_data[point - region:point + region])
 
-        fft = np.fft.fft(fragment_d1)
-        fft_v = fft.real ** 2 + fft.imag ** 2
-        filter_values = np.vectorize(lambda x: down_to_zero(x, edge=fft_v.max() * 0.05))
-        fft_v_filter = filter_values(fft_v)
+            fft = np.fft.fft(fragment_d1)
+            fft_v = fft.real ** 2 + fft.imag ** 2
+            filter_values = np.vectorize(lambda x: down_to_zero(x, edge=fft_v.max() * 0.05))
+            fft_v_filter = filter_values(fft_v)
 
-        frequency = np.unique(np.abs(np.fft.fftfreq(fragment_d1.shape[0])))
-        frequency = frequency[(frequency >= 0.0) & (frequency <= 0.1)]
-        fft_v_filter = fft_v_filter[:frequency.shape[0]]
+            frequency = np.unique(np.abs(np.fft.fftfreq(fragment_d1.shape[0])))
+            frequency = frequency[(frequency >= 0.0) & (frequency <= 0.1)]
+            fft_v_filter = fft_v_filter[:frequency.shape[0]]
 
-        max_fft, max_ratio_fft, fr_max_fft = count_fft_max(fft_v_filter, frequency)
+            max_fft, max_ratio_fft, fr_max_fft = count_fft_max(fft_v_filter, frequency)
 
-        abs_skewness = np.abs(skew(fragment_d1))
+            abs_skewness = np.abs(skew(fragment_d1))
 
-        if 0 < max_fft <= MAX_FFT_MAX and periods_d1 >= MIN_PERIODS and periods <= MAX_PERIODS and max_ratio_fft < MAX_RATIO_FFT and abs_skewness < MAX_SKEWNESS:
+            if 0 < max_fft <= MAX_FFT_MAX and periods_d1 >= MIN_PERIODS and periods <= MAX_PERIODS and max_ratio_fft < MAX_RATIO_FFT and abs_skewness < MAX_SKEWNESS:
+                f_point = True
+
+        if f_point:
             f_fragment = True
-
             preprocessed_ind_data[-1].append(point)
-            signal_data_f[point] = 0.1
             search_step = 1
         else:
             search_step = region // 2
@@ -301,12 +227,8 @@ def fft_butter_skewness_filtering_new(t_data, signal_data, rate, log_df=None, f_
             iter_count += 1
             print(".", end="")
 
-    if log_df is not None:
-        log_df["ch11_f"] = signal_data_f
-
     preprocessed_ind_data = length_preproc(preprocessed_ind_data, rate, SHRED_LENGTH_MCS=REGION_LENGTH_MCS * 1.2)
-    fragments = [[], []]
-    signal_data_f = np.zeros(signal_data.shape[0])
+    fragments = [[]] + [[] for i in range(CHANNELS_N)]
 
     iter_step = len(preprocessed_ind_data) // 5
     iter_count = 1
@@ -318,15 +240,12 @@ def fft_butter_skewness_filtering_new(t_data, signal_data, rate, log_df=None, f_
         r_edge = min(fragment_ind[fragment_len - 1] + int(BOARDERS_PERCENT * fragment_len), t_data.shape[0] - 1)
 
         fragments[0].append(t_data[l_edge:r_edge + 1])
-        fragments[1].append(signal_data[l_edge:r_edge + 1])
+        for ch_i in range(CHANNELS_N):
+            fragments[ch_i + 1].append(signal_data[ch_i][l_edge:r_edge + 1])
 
-        signal_data_f[l_edge:r_edge + 1] = 0.2
         if f_disp and iter_step * iter_count <= i:
             iter_count += 1
             print(".", end="")
-
-    if log_df is not None:
-        log_df["ch11_f_fin"] = signal_data_f
 
     if f_disp:
         print("|")
